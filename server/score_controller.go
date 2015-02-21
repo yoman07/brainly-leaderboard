0fb931f9163ab8c27162af40cecfe371caefa02d
k3nn7 <lukasz.lalik@gmail.com>
2015-02-21 01:05:20 +0100

    Score action also displays all users data

--- server/score_controller.go
+++ server/score_controller.go
@@ -1,12 +1,23 @@
 package main
 
 import (
+    "github.com/k3nn7/leaderboard/leaderboard"
     "net/http"
     "fmt"
 )
 
 func ScoreIndex(w http.ResponseWriter, r *http.Request) {
     ranking, _ := application.ContestService.GetRanking()
+
+    userIds, _ := application.UserService.GetAllUsersIds()
+    users := make([]leaderboard.User, 0)
+
+    for _, userId := range userIds {
+        user, _ := application.UserService.GetUser(userId)
+        users = append(users, user)
+    }
+
+    fmt.Fprintf(w, "Users: %v", users)
 
 
     fmt.Fprintf(w, "Ranking: %v", ranking)
